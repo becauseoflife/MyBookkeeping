11fac2e47e62f46bcc8cfedb98d6fa0fc27df4c9
becauseoflife <1752196851@qq.com>
2020-03-17 00:19:34 +0800

    修改密码功能

--- pages/changepwd/changepwd.js
+++ pages/changepwd/changepwd.js
@@ -1,4 +1,5 @@
 // pages/changepwd/changepwd.js
+var app = getApp();
 Page({
 
   /**
@@ -10,6 +11,80 @@ Page({
 
   changeSubmit: function(e){
     console.log(e.detail.value);
+    var formData = e.detail.value;
+    var {oldPwd, newPwd, okPwd} = formData;
+    var isPostData = false;
+    var hint = '';
+    if(!oldPwd){
+      hint = '请输入原密码'
+    }else if(!newPwd){
+      hint = '请输入新密码'
+    }else if(!okPwd){
+      hint = '请确认新密码'
+    }
+    if (hint) {
+      wx.showToast({
+        title: hint,
+        icon: 'none',
+        duration: 1000
+      })
+      return;
+    }
+    // 判断两次输入密码是否一样
+    if (!hint && (newPwd !== okPwd)) {
+      hint = '两次输入的密码不一样'
+    } else if (!hint && (oldPwd === newPwd)){
+      hint = '新密码和原密码相同'
+    } else {
+      isPostData = true;
+    }
+    wx.showToast({
+      title: hint,
+      icon: 'none',
+      duration: 1000
+    })
+
+    // 上传到服务器
+    if(isPostData){
+      wx.request({
+        url: 'http://192.168.1.89:8080/userOperation/changePassword',
+        method: 'POST',
+        header: {
+          'content-type': 'application/x-www-form-urlencoded',
+          'sessionId': app.globalData.sessionId
+        },
+        data: {
+          'oldPwd': oldPwd,
+          'newPwd': newPwd
+        },
+        success: function(res){
+          console.log(res.data)
+          var resData = res.data;
+          if(resData.status == 200){
+            wx.showToast({
+              title: resData.msg,
+              icon: 'success',
+              duration: 1500
+            })
+            // 跳转回用户界面
+            setTimeout(function () {
+              wx.switchTab({
+                url: '../user/user',
+              })
+            }, 1500);
+          }else if(resData.status == 500){
+            wx.showToast({
+              title: resData.msg,
+              icon: 'none',
+              duration: 1000
+            })
+          }
+        },
+        fail: function(res){
+          console.log(res.data)
+        }
+      })
+    }
   },
   /**
    * 生命周期函数--监听页面加载
